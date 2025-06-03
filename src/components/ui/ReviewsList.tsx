059abecfd1380259dea389c747be91eec9480b4b
ghassen-kharrat <kharrat.ghassen@gmail.com>
2025-06-03 02:40:54 +0100

    Replace React icons with SVG elements in review components

--- src/components/ui/ReviewsList.tsx
+++ src/components/ui/ReviewsList.tsx
@@ -1,8 +1,39 @@
 import React from 'react';
 import { styled } from 'styled-components';
-import { FaStar, FaStarHalf, FaUser, FaCheck, FaCommentSlash } from 'react-icons/fa';
 import { ReviewData } from '../../features/products/services/types';
 import { useLanguage } from '../../provider/LanguageProvider';
+
+// Custom SVG icons
+const StarSVG = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+  </svg>
+);
+
+const StarHalfSVG = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+    <path d="M5.354 5.119 7.538.792A.516.516 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.537.537 0 0 1 16 6.32a.548.548 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.52.52 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.58.58 0 0 1 .085-.302.513.513 0 0 1 .37-.245l4.898-.696zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.565.565 0 0 1 .162-.505l2.907-2.77-4.052-.576a.525.525 0 0 1-.393-.288L8.001 2.223 8 2.226v9.8z"/>
+  </svg>
+);
+
+const UserSVG = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c0-.001-0-1.001-5.006-1.001C3.984 11.995 3.99 12.004 4 13h10z"/>
+  </svg>
+);
+
+const CheckSVG = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" viewBox="0 0 16 16">
+    <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+  </svg>
+);
+
+const CommentSlashSVG = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+    <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+    <path d="M3.707 3.293a1 1 0 0 1 1.414 0L8 6.586l2.879-2.293a1 1 0 0 1 1.414 1.414L9.414 8l2.879 2.293a1 1 0 0 1-1.414 1.414L8 9.414l-2.879 2.293a1 1 0 0 1-1.414-1.414L6.586 8 3.707 5.707a1 1 0 0 1 0-1.414z"/>
+  </svg>
+);
 
 interface ReviewsListProps {
   reviews: ReviewData[];
@@ -82,15 +113,15 @@ const StyledEmptyStarIcon = styled.span`
 `;
 
 const StarIcon: React.FC = () => (
-  <StyledStarIcon>{React.createElement(FaStar)}</StyledStarIcon>
+  <StyledStarIcon><StarSVG /></StyledStarIcon>
 );
 
 const HalfStarIcon: React.FC = () => (
-  <StyledHalfStarIcon>{React.createElement(FaStarHalf)}</StyledHalfStarIcon>
+  <StyledHalfStarIcon><StarHalfSVG /></StyledHalfStarIcon>
 );
 
 const EmptyStarIcon: React.FC = () => (
-  <StyledEmptyStarIcon>{React.createElement(FaStar)}</StyledEmptyStarIcon>
+  <StyledEmptyStarIcon><StarSVG /></StyledEmptyStarIcon>
 );
 
 const RatingBars = styled.div`
@@ -395,7 +426,9 @@ const ReviewsList: React.FC<ReviewsListProps> = ({
       
       {safeReviews.length === 0 ? (
         <EmptyMessage>
-          {React.createElement(FaCommentSlash)}
+          <EmptyStateIcon>
+            <CommentSlashSVG />
+          </EmptyStateIcon>
           <div>{t('no_reviews')} {t('be_first')}</div>
         </EmptyMessage>
       ) : (
@@ -406,14 +439,14 @@ const ReviewsList: React.FC<ReviewsListProps> = ({
                 <ReviewHeader>
                   <ReviewAuthor>
                     <AuthorIcon>
-                      {React.createElement(FaUser)}
+                      <UserSVG />
                     </AuthorIcon>
                     <AuthorInfo>
                       <AuthorName>
                         {review.firstName ? `${review.firstName} ${review.lastName?.charAt(0)}.` : t('user')}
                         {review.isVerified && (
                           <VerifiedBadge>
-                            {React.createElement(FaCheck, { style: { marginRight: '0.3rem' } })} {t('verified_purchase')}
+                            <CheckSVG /> {t('verified_purchase')}
                           </VerifiedBadge>
                         )}
                       </AuthorName>
